049d600a588484470c0add3c38a8e7a62d6ac80f
Shady Alfred <shico2000@gmail.com>
2018-11-25 18:06:08 +0200

    Metro lists

--- Metro.py
+++ Metro.py
@@ -1,43 +1,32 @@
-class Station:
+def count(start, end):
+    # Defining line 1
+    line1 = {}
+    list1 = ['Shohada', 'Oraby', 'Gamal Abdel Nasser', 'Sadat', 'Saad Zaghlol', 'Zeynab']
+    for i, station in enumerate(list1):
+        line1[station] = i
 
-    def __init__(self, name = None, n = None, p = None, isTrans = None):
-        self.name = name
-        self.n = n
-        self.p = p
-        self.isTrans = isTrans
+    # Defining line 2
+    line2 = {}
+    list2 = ['Shohada', 'Ataba', 'Mohammad Naguib', 'Sadat', 'Opera', 'Dokki']
+    for i, station in enumerate(list2):
+        line2[station] = i
 
-        if isTrans:
-            self.n1 = None
-            self.p1 = None
+    if start in line1 and end in line1:
+        return abs(line1[start] - line1[end])
+    elif start in line2 and end in line2:
+        return abs(line2[start] - line2[end])
+    elif start in line1 and end in line2:
+        shohadaDist = abs(line1[start] - line1['Shohada'])
+        sadatDist = abs(line1[start] - line1['Sadat'])
+        if shohadaDist < sadatDist:
+            nearestTransDist = (shohadaDist, 'Shohada')
+        else:
+            nearestTransDist = (sadatDist, 'Sadat')
 
+        return nearestTransDist[0] + abs(line2[nearestTransDist[1]] - line2[end])
 
-class Line:
-
-    def __init__(self, station):
-        self.head = station
-        self.currentStation = self.head
-
-    def append(self, nextStation):
-        self.currentStation.n = nextStation
-        nextStation.p = self.currentStation
-        self.currentStation = nextStation
-
-    def count(self, startStation, endStation):
-        c = 0
-        this = self.head
-        while this.name != startStation:
-            this = this.n
-        while this.name != endStation:
-            this = this.n
-            c += 1
-        return c
+    
 
 
-line1List = ['Mohammad Naguib', 'Sadat', 'Opera', 'Dokki']
-line1 = Line(Station('Ataba'))
-for element in line1List:
-    line1.append(Station(element))
 
-print(line1.head.name)
-print(line1.currentStation.name)
-print(line1.count('Dokki', 'Sadat'))
+print(count('Shohada', 'Zeynab'))
